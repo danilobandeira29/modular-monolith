441fb86d1cf36644de2d3fb3e8e4d58fbbed1b72
Danilo Bandeira <danilobandeira29@gmail.com>
2023-08-15 07:17:45 -0300

    feat: initial commit

---
+++ src/modules/@shared/domain/entity/entity.ts
@@ -0,0 +1,29 @@
+import Id from "../value-object/id-object";
+
+export default class Entity {
+    private readonly _id: Id;
+    private readonly _createdAt: Date;
+    private _updatedAt: Date;
+
+    constructor(id?: Id) {
+        this._id = id || new Id();
+        this._createdAt = new Date();
+        this._updatedAt = new Date();
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get createdAt() {
+        return this._createdAt;
+    }
+
+    get updateAt() {
+        return this._updatedAt;
+    }
+
+    set updateAt(d: Date) {
+        this._updatedAt = d;
+    }
+}

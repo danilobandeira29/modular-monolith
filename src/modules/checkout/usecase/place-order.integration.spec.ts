caf0905105efead1f7a30e1fb9a537327d1ca81f
Danilo Bandeira <danilobandeira29@gmail.com>
2023-09-19 07:25:52 -0300

    test: should place a order and status be 'pending' because order's total is below one hundred and invoiceId be null

--- src/modules/checkout/usecase/place-order.integration.spec.ts
+++ src/modules/checkout/usecase/place-order.integration.spec.ts
@@ -33,27 +33,88 @@ describe("Place Order Integration Tests", () => {
         await sequelize.close();
     });
 
-    it("should place a order", async () => {
+    it("should place a order and status be 'approved' because order's total is more or equal to one hundred and contains invoiceId", async () => {
         const clientAdm = ClientAdmFacadeFactory.create();
         const clientId = new Id().toString();
         await clientAdm.addClient({ id: clientId, document: '1' ,name: 'Danilo Bandeira', email: 'email@email.com', address: { street: 'street', zipCode: '1', number: '1', state: 'state', complement: 'complement', city: 'city' } });
         const productAdm = ProductAdmFactoryFacade.create();
         const productId = new Id().toString();
+        const productId2 = new Id().toString();
+        const productId3 = new Id().toString();
+        const productId4 = new Id().toString();
         await productAdm.addProduct({ id: productId, name: '', stock: 1, purchasePrice: 100, description: '' });
+        await productAdm.addProduct({ id: productId2, name: '', stock: 2, purchasePrice: 100, description: '' });
+        await productAdm.addProduct({ id: productId3, name: '', stock: 10, purchasePrice: 100, description: '' });
+        await productAdm.addProduct({ id: productId4, name: '', stock: 200, purchasePrice: 100, description: '' });
         const storeCatalogAdm = StoreCatalogFactoryFacade.create();
         const payment = PaymentFactory.create();
         const invoice = new InvoiceRepository();
         const order = new OrderRepository();
         const useCase = new PlaceOrderUseCase({ clientAdm, productAdm, storeCatalogAdm, payment, invoice, order });
-        const result = await useCase.execute({ clientId: clientId, products: [{productId}] });
+        const result = await useCase.execute({ clientId: clientId, products: [{productId}, {productId: productId2}, {productId: productId3}, {productId: productId4}] });
         expect(result).toStrictEqual({
             id: expect.any(String),
             invoiceId: expect.any(String),
-            products: [{ productId: expect.any(String) }],
+            products: [
+                {
+                    productId: productId
+                },
+                {
+                    productId: productId2
+                },
+                {
+                    productId: productId3
+                },
+                {
+                    productId: productId4
+                },
+            ],
             status: "approved",
-            total: 100
+            total: 400
         });
     })
+
+    it("should place a order and status be 'pending' because order's total is below one hundred and invoiceId be null", async () => {
+        const clientAdm = ClientAdmFacadeFactory.create();
+        const clientId = new Id().toString();
+        await clientAdm.addClient({ id: clientId, document: '1' ,name: 'Danilo Bandeira', email: 'email@email.com', address: { street: 'street', zipCode: '1', number: '1', state: 'state', complement: 'complement', city: 'city' } });
+        const productAdm = ProductAdmFactoryFacade.create();
+        const productId = new Id().toString();
+        const productId2 = new Id().toString();
+        const productId3 = new Id().toString();
+        const productId4 = new Id().toString();
+        await productAdm.addProduct({ id: productId, name: '', stock: 1, purchasePrice: 10, description: '' });
+        await productAdm.addProduct({ id: productId2, name: '', stock: 2, purchasePrice: 5, description: '' });
+        await productAdm.addProduct({ id: productId3, name: '', stock: 10, purchasePrice: 2, description: '' });
+        await productAdm.addProduct({ id: productId4, name: '', stock: 200, purchasePrice: 3, description: '' });
+        const storeCatalogAdm = StoreCatalogFactoryFacade.create();
+        const payment = PaymentFactory.create();
+        const invoice = new InvoiceRepository();
+        const order = new OrderRepository();
+        const useCase = new PlaceOrderUseCase({ clientAdm, productAdm, storeCatalogAdm, payment, invoice, order });
+        const result = await useCase.execute({ clientId: clientId, products: [{productId}, {productId: productId2}, {productId: productId3}, {productId: productId4}] });
+        expect(result).toStrictEqual({
+            id: expect.any(String),
+            invoiceId: null,
+            products: [
+                {
+                    productId: productId
+                },
+                {
+                    productId: productId2
+                },
+                {
+                    productId: productId3
+                },
+                {
+                    productId: productId4
+                },
+            ],
+            status: "pending",
+            total: 20
+        });
+    })
+
 
     it("should throw errors when products are empty", async () => {
         const clientAdm = ClientAdmFacadeFactory.create();
@@ -88,5 +149,4 @@ describe("Place Order Integration Tests", () => {
         await productAdm.addProduct({ id: productId4, name: '', stock: 0, purchasePrice: 100, description: '' });
         await expect((() => useCase.execute({ clientId: clientId, products: [{ productId }, { productId: productId2 }, { productId: productId3 }, { productId: productId4 }] }))).rejects.toThrowError(/must be in Stock/);
     })
-
 })
